6d25b1091731981f264620ede641f3c200b37363
Diego Camarmas <dcamarma@inf.uc3m.es>
2024-10-28 18:03:37 +0100

    2024 minor updates

--- ec_p1_corrector/mk_solution.sh
+++ ec_p1_corrector/mk_solution.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#set -x
+set -x
 
 TEST_1=$(ls -1 test/ej1 | grep -v "\.o")
 TEST_2=$(ls -1 test/ej2 | grep -v "\.o")
-TEST_3=$(ls -1 test/ej3 | grep -v "\.o")
 
 mkdir -p solution/test
 mkdir -p solution/output
@@ -15,7 +14,7 @@ for T in $TEST_1; do
 	#Generar salida de solución correcta
 	cat solution/ejercicio1.s | grep -v ".text" > /tmp/$$.txt
 	cat ./test/ej1/$T /tmp/$$.txt > solution/test/test_ejercicio1_$T
-	/creator/creator.sh -a "/creator/architecture/RISC_V_RV32IMFD.json" -s solution/test/test_ejercicio1_$T -o min > solution/output/output_ejercicio1_$T.txt
+	/creator/creator.sh -a "/creator/architecture/RISC_V_RV32IMFD.json" -s solution/test/test_ejercicio1_$T -l ./test/pow.o -o min > solution/output/output_ejercicio1_$T.txt
 
 	#Delete stack TODO if memory
 	exits=$(grep "memory" solution/output/output_ejercicio1_$T.txt)
@@ -36,17 +35,23 @@ for T in $TEST_1; do
 
 	#Delete registers, display and keyboard
 	sed 's/cr[^;]*;\s//g' solution/output/aux_output_ejercicio1_$T.txt | \
-	sed 's/ir\[t[0-9][^;]*;\s//g' | \
-	sed 's/sfpr\[f[4-9][^;]*;\s//g' | \
-	sed 's/sfpr\[f10[^;]*;\s//g' | \
-	sed 's/dfpr[^;]*;\s//g' | \
-	sed 's/sfpr\[f11[^;]*;\s//g' | \
-	sed 's/ir\[ra[^;]*;\s//g' | \
-	sed 's/keyboard[^;]*;\s//g' | \
-	sed 's/display[^;]*;\s//g' | \
-	sed 's/ir\[at[^;]*;\s//g' | \
-	sed 's/ir\[fp[^;]*;\s//g' | \
-	sed 's/ir\[a[1-7][^;]*;\s//g' > solution/output/output_ejercicio1_$T.txt 
+	sed 's/ir\[x1[^;]*;\s//g' | \
+	sed 's/ir\[x3[^;]*;\s//g' | \
+	sed 's/ir\[x4[^;]*;\s//g' | \
+	sed 's/ir\[x8[^;]*;\s//g' | \
+	sed 's/ir\[x[5-7][^;]*;\s//g' | \
+	sed 's/ir\[x28[^;]*;\s//g' | \
+	sed 's/ir\[x29[^;]*;\s//g' | \
+	sed 's/ir\[x30[^;]*;\s//g' | \
+	sed 's/ir\[x31[^;]*;\s//g' | \
+	sed 's/ir\[x1[0-7][^;]*;\s//g' | \
+	sed 's/fpr\[f[0-7][^;]*;\s//g' | \
+	sed 's/fpr\[f28[^;]*;\s//g' | \
+	sed 's/fpr\[f29[^;]*;\s//g' | \
+	sed 's/fpr\[f30[^;]*;\s//g' | \
+	sed 's/fpr\[f31[^;]*;\s//g' | \
+	sed 's/fpr\[f1[0-7][^;]*;\s//g' | \
+	sed 's/keyboard[^;]*;\s//g' > solution/output/output_ejercicio1_$T.txt
 
 	rm solution/output/aux_output_ejercicio1_$T.txt
 done
@@ -57,7 +62,7 @@ for T in $TEST_2; do
 	#Generar salida de solución correcta
 	cat solution/ejercicio2.s | grep -v ".text" > /tmp/$$.txt
 	cat ./test/ej2/$T /tmp/$$.txt > solution/test/test_ejercicio2_$T
-	/creator/creator.sh -a "/creator/architecture/RISC_V_RV32IMFD.json" -s solution/test/test_ejercicio2_$T -o min > solution/output/output_ejercicio2_$T.txt
+	/creator/creator.sh -a "/creator/architecture/RISC_V_RV32IMFD.json" -s solution/test/test_ejercicio2_$T -l ./test/pow.o -o min > solution/output/output_ejercicio2_$T.txt
 
 	#Delete stack TODO if memory
 	exits=$(grep "memory" solution/output/output_ejercicio2_$T.txt)
@@ -78,59 +83,24 @@ for T in $TEST_2; do
 
 	#Delete registers, display and keyboard
 	sed 's/cr[^;]*;\s//g' solution/output/aux_output_ejercicio2_$T.txt | \
-	sed 's/ir\[t[0-9][^;]*;\s//g' | \
-	sed 's/sfpr\[f[4-9][^;]*;\s//g' | \
-	sed 's/sfpr\[f10[^;]*;\s//g' | \
-	sed 's/dfpr[^;]*;\s//g' | \
-	sed 's/sfpr\[f11[^;]*;\s//g' | \
-	sed 's/ir\[ra[^;]*;\s//g' | \
-	sed 's/keyboard[^;]*;\s//g' | \
+	sed 's/ir\[x1[^;]*;\s//g' | \
+	sed 's/ir\[x3[^;]*;\s//g' | \
+	sed 's/ir\[x4[^;]*;\s//g' | \
+	sed 's/ir\[x8[^;]*;\s//g' | \
+	sed 's/ir\[x[5-7][^;]*;\s//g' | \
+	sed 's/ir\[x28[^;]*;\s//g' | \
+	sed 's/ir\[x29[^;]*;\s//g' | \
+	sed 's/ir\[x30[^;]*;\s//g' | \
+	sed 's/ir\[x31[^;]*;\s//g' | \
+	sed 's/ir\[x1[0-7][^;]*;\s//g' | \
+	sed 's/fpr\[f[0-7][^;]*;\s//g' | \
+	sed 's/fpr\[f28[^;]*;\s//g' | \
+	sed 's/fpr\[f29[^;]*;\s//g' | \
+	sed 's/fpr\[f30[^;]*;\s//g' | \
+	sed 's/fpr\[f31[^;]*;\s//g' | \
+	sed 's/fpr\[f1[0-7][^;]*;\s//g' | \
 	sed 's/display[^;]*;\s//g' | \
-	sed 's/ir\[at[^;]*;\s//g' | \
-	sed 's/ir\[fp[^;]*;\s//g' | \
-	sed 's/ir\[a[0-7][^;]*;\s//g' > solution/output/output_ejercicio2_$T.txt 
+	sed 's/keyboard[^;]*;\s//g' > solution/output/output_ejercicio2_$T.txt 
 
 	rm solution/output/aux_output_ejercicio2_$T.txt
-done
-
-for T in $TEST_3; do
-	echo $T
-	
-	#Generar salida de solución correcta
-	cat solution/ejercicio3.s | grep -v ".text" > /tmp/$$.txt
-	cat ./test/ej3/$T /tmp/$$.txt > solution/test/test_ejercicio3_$T
-	/creator/creator.sh -a "/creator/architecture/RISC_V_RV32IMFD.json" -s solution/test/test_ejercicio3_$T -o min > solution/output/output_ejercicio3_$T.txt
-
-	#Delete stack TODO if memory
-	exits=$(grep "memory" solution/output/output_ejercicio3_$T.txt)
-
-	if [ -n "${exits}" ]
-	then
-		mem=$(sed 's/.*memory\[\([^:]*\)\]:[^;]*;.*/\1/' solution/output/output_ejercicio3_$T.txt | tail -n 2 | sed ':a;N;$!ba;s/\n//g')
-		
-		if [ $(($mem)) -le $((0x0FFFFFFF)) ] && [ $(($mem)) -ge $((0x05BBFCBF)) ]
-		then
-			sed "s/memory\[$mem\][^;]*;\s//g" solution/output/output_ejercicio3_$T.txt > solution/output/aux_output_ejercicio3_$T.txt
-		else
-			cp solution/output/output_ejercicio3_$T.txt solution/output/aux_output_ejercicio3_$T.txt
-		fi
-	else 
-		cp solution/output/output_ejercicio3_$T.txt solution/output/aux_output_ejercicio3_$T.txt
-	fi
-
-	#Delete registers, display and keyboard
-	sed 's/cr[^;]*;\s//g' solution/output/aux_output_ejercicio3_$T.txt | \
-	sed 's/ir\[t[0-9][^;]*;\s//g' | \
-	sed 's/sfpr\[f[4-9][^;]*;\s//g' | \
-	sed 's/sfpr\[f10[^;]*;\s//g' | \
-	sed 's/dfpr[^;]*;\s//g' | \
-	sed 's/sfpr\[f11[^;]*;\s//g' | \
-	sed 's/ir\[ra[^;]*;\s//g' | \
-	sed 's/keyboard[^;]*;\s//g' | \
-	sed 's/display[^;]*;\s//g' | \
-	sed 's/ir\[at[^;]*;\s//g' | \
-	sed 's/ir\[fp[^;]*;\s//g' | \
-	sed 's/ir\[a[0-7][^;]*;\s//g' > solution/output/output_ejercicio3_$T.txt 
-
-	rm solution/output/aux_output_ejercicio3_$T.txt
 done
