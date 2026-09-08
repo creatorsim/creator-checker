35d368cbb45f1e5a20c2766ea3894c07dc177029
Diego Camarmas-Alonso <dcamarma@inf.uc3m.es>
2026-09-08 15:06:45 +0200

    Minor updates

--- scripts/s30_checker.sh
+++ scripts/s30_checker.sh
@@ -24,7 +24,7 @@ if [ -f "$TXTFILE" ]; then
     echo "Using existing list: $TXTFILE"
 else
     echo "Creating new list: $TXTFILE"
-    ls -1 "/workdir/submissions/$GROUP" | sed 's/.zip$//' | grep -v index.html | sort | uniq > "$TXTFILE"
+    ls -1 "/workdir/submissions/$GROUP" | sed 's/.zip$//' | grep -v index_$GROUP.html | sort | uniq > "$TXTFILE"
 fi
 
 # Read directories to process from the list file
@@ -33,46 +33,42 @@ LIST=$(cat "$TXTFILE" | tr -d '\r' | grep -v '^$')
 TEST_1=$(ls -1 /workdir/tests/exercise_1 | grep -v "\.o") # TODO: generic
 
 # Header
-rm -fr /workdir/results/grades_$GROUP.csv
-touch  /workdir/results/grades_$GROUP.csv
+rm    -rf /workdir/results/$GROUP
+mkdir -p  /workdir/results/$GROUP
+touch     /workdir/results/$GROUP/grades_$GROUP.csv
+#####
+touch             /workdir/results/$GROUP/index_$GROUP.html
+echo "<html>" >>  /workdir/results/$GROUP/index_$GROUP.html
+#####
 
-echo -n "Group;" >> /workdir/results/grades_$GROUP.csv
+
+echo -n "Group;" >> /workdir/results/$GROUP/grades_$GROUP.csv
 
 for T in $TEST_1; do
-    echo -n $T";" >> /workdir/results/grades_$GROUP.csv
+    echo -n $T";" >> /workdir/results/$GROUP/grades_$GROUP.csv
 done
 
-echo "" >> /workdir/results/grades_$GROUP.csv
+echo "" >> /workdir/results/$GROUP/grades_$GROUP.csv
 
-#####
-rm -fr            /workdir/submissions/$GROUP/index.html
-touch             /workdir/submissions/$GROUP/index.html
-echo "<html>" >>  /workdir/submissions/$GROUP/index.html
-#####
 
 # Grades
 for E in $LIST; do
 
     echo " ### $E ###################### "
-    echo -n $E";" >> /workdir/results/grades_$GROUP.csv
+    echo -n $E";" >> /workdir/results/$GROUP/grades_$GROUP.csv
 
-    rm -fr /workdir/submissions/$GROUP/$E/test
-    rm -fr /workdir/submissions/$GROUP/$E/test_output
-    rm -fr /workdir/submissions/$GROUP/$E/test_problems
-    rm -fr /workdir/submissions/$GROUP/$E/logs.txt
-
-    mkdir -p /workdir/submissions/$GROUP/$E/test
-    mkdir -p /workdir/submissions/$GROUP/$E/test_output
-
+    mkdir -p /workdir/results/$GROUP/$E
+    mkdir -p /workdir/results/$GROUP/$E/test
+    mkdir -p /workdir/results/$GROUP/$E/test_output
     #######
-    echo "<table>"           >> /workdir/submissions/$GROUP/index.html
-    echo "<tr>"              >> /workdir/submissions/$GROUP/index.html
-    echo "<td>Group</td>"    >> /workdir/submissions/$GROUP/index.html
-    echo "<td>Test</td>"     >> /workdir/submissions/$GROUP/index.html
-    echo "<td>OK</td>"       >> /workdir/submissions/$GROUP/index.html
-    echo "<td>Output</td>"   >> /workdir/submissions/$GROUP/index.html
-    echo "<td>Src</td>"      >> /workdir/submissions/$GROUP/index.html
-    echo "</tr>"             >> /workdir/submissions/$GROUP/index.html
+    echo "<table>"           >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<tr>"              >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>Group</td>"    >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>Test</td>"     >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>OK</td>"       >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>Output</td>"   >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>Src</td>"      >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "</tr>"             >> /workdir/results/$GROUP/index_$GROUP.html
     #######
 
     for T in $TEST_1; do
@@ -80,12 +76,12 @@ for E in $LIST; do
         echo -n $T" "
 
         # Build test code
-        cat /workdir/submissions/$GROUP/$E/${ENAME1}.s | \
+        cat /workdir/results/$GROUP/$E/${ENAME1}.s | \
         sed 's/\.text/ /gi'     | \
         sed 's/\.data/ /gi'     | \
         sed 's/\bmain:/main_student:/gi' > /tmp/$$.txt
 
-        cat ./test/exercise_1/$T /tmp/$$.txt  >  /workdir/submissions/$GROUP/$E/test/test_${ENAME1}_$T # TODO: generic
+        cat ./test/exercise_1/$T /tmp/$$.txt  >  /workdir/results/$GROUP/$E/test/test_${ENAME1}_$T # TODO: generic
 
 
 
@@ -96,28 +92,33 @@ for E in $LIST; do
 
 
         # CREATOR...
-        /creator/creator.sh -a $ARCH_PATH -s /workdir/submissions/$GROUP/$E/test/test_${ENAME1}_$T --validate /workdir/tests/solution/output_${ENAME1}_$T.yml > /tmp/$$.txt
+        # /creator/creator.sh -a $ARCH_PATH -s /workdir/results/$GROUP/$E/test/test_${ENAME1}_$T --validate /workdir/tests/solution/output_${ENAME1}_$T.yml > /tmp/$$.txt
 
-        if [ $? -eq 0 ]
-        then
-           echo -n "1;" >> /workdir/results/grades_$GROUP.csv
-           OK=1
-        else
-           echo -n "0;" >> /workdir/results/grades_$GROUP.csv
-           OK=0
+        # if [ $? -eq 0 ]
+        # then
+        #    echo -n "1;" >> /workdir/results/$GROUP/grades_$GROUP.csv
+        #    OK=1
+        # else
+        #    echo -n "0;" >> /workdir/results/$GROUP/grades_$GROUP.csv
+        #    OK=0
 
-            mkdir -p /workdir/submissions/$GROUP/$E/test_problems
+        #     mkdir -p /workdir/results/$GROUP/$E/test_problems
 
-            /creator/creator.sh -a $ARCH_PATH -s /workdir/submissions/$GROUP/$E/test/test_${ENAME1}_$T --validate /workdir/tests/solution/output_${ENAME1}_$T.yml &> /workdir/submissions/$GROUP/$E/test_problems/problem_${ENAME1}_$T.txt
+        #     /creator/creator.sh -a $ARCH_PATH -s /workdir/results/$GROUP/$E/test/test_${ENAME1}_$T --validate /workdir/tests/solution/output_${ENAME1}_$T.yml &> /workdir/results/$GROUP/$E/test_problems/problem_${ENAME1}_$T.txt
 
-            #/creator/creator.sh -a "/creator/architecture/RISC_V_RV32IMFD.json" -s /workdir/submissions/$GROUP/$E/test/test_${ENAME1}_$T | aha > /workdir/submissions/$GROUP/$E/test_problems/problem_${ENAME1}_$T.html
-            #wkhtmltopdf /workdir/submissions/$GROUP/$E/test_problems/problem_${ENAME1}_$T.html /workdir/submissions/$GROUP/$E/test_problems/problem_${ENAME1}_$T.pdf &> /dev/null
-        fi
+            #/creator/creator.sh -a "/creator/architecture/RISC_V_RV32IMFD.json" -s /workdir/results/$GROUP/$E/test/test_${ENAME1}_$T | aha > /workdir/results/$GROUP/$E/test_problems/problem_${ENAME1}_$T.html
+            #wkhtmltopdf /workdir/results/$GROUP/$E/test_problems/problem_${ENAME1}_$T.html /workdir/results/$GROUP/$E/test_problems/problem_${ENAME1}_$T.pdf &> /dev/null
+        # fi
 
-        cp /workdir/submissions/$GROUP/$E/test/test_${ENAME1}_$T /workdir/submissions/$GROUP/$E/test_output/e1_$T.txt
-        cp /tmp/$$.txt                                           /workdir/submissions/$GROUP/$E/test_output/s1_$T.txt
 
-        cat /tmp/$$.txt >> /workdir/submissions/$GROUP/$E/logs.txt
+
+
+
+
+        cp /workdir/results/$GROUP/$E/test/test_${ENAME1}_$T /workdir/results/$GROUP/$E/test_output/e1_$T.txt
+        cp /tmp/$$.txt                                       /workdir/results/$GROUP/$E/test_output/s1_$T.txt
+
+        cat /tmp/$$.txt >> /workdir/results/$GROUP/$E/logs.txt
         cat /tmp/$$.txt
         rm  /tmp/$$.txt
 
@@ -125,35 +126,35 @@ for E in $LIST; do
 
 
         #######
-        echo ""                                                             >> /workdir/submissions/$GROUP/index.html
-        echo "<tr>"                                                         >> /workdir/submissions/$GROUP/index.html
-        echo "<td>$E</td>"                                                  >> /workdir/submissions/$GROUP/index.html
-        echo "<td>$T</td>"                                                  >> /workdir/submissions/$GROUP/index.html
-        echo "<td>$OK</td>"                                                 >> /workdir/submissions/$GROUP/index.html
-        echo "<td><a href=\"./$E/test_output/s1_$T.txt\">link</a></td>"     >> /workdir/submissions/$GROUP/index.html
-        echo "<td><a href=\"./$E/test_output/e1_$T.txt\">link</a></td>"     >> /workdir/submissions/$GROUP/index.html
-        echo "</tr>"                                                        >> /workdir/submissions/$GROUP/index.html
+        echo ""                                                             >> /workdir/results/$GROUP/index_$GROUP.html
+        echo "<tr>"                                                         >> /workdir/results/$GROUP/index_$GROUP.html
+        echo "<td>$E</td>"                                                  >> /workdir/results/$GROUP/index_$GROUP.html
+        echo "<td>$T</td>"                                                  >> /workdir/results/$GROUP/index_$GROUP.html
+        echo "<td>$OK</td>"                                                 >> /workdir/results/$GROUP/index_$GROUP.html
+        echo "<td><a href=\"./$E/test_output/s1_$T.txt\">link</a></td>"     >> /workdir/results/$GROUP/index_$GROUP.html
+        echo "<td><a href=\"./$E/test_output/e1_$T.txt\">link</a></td>"     >> /workdir/results/$GROUP/index_$GROUP.html
+        echo "</tr>"                                                        >> /workdir/results/$GROUP/index_$GROUP.html
         #######
     done
 
 
     #######
-    echo "<tr>"              >> /workdir/submissions/$GROUP/index.html
-    echo "<td>Group</td>"    >> /workdir/submissions/$GROUP/index.html
-    echo "<td>Test</td>"     >> /workdir/submissions/$GROUP/index.html
-    echo "<td>OK</td>"       >> /workdir/submissions/$GROUP/index.html
-    echo "<td>Output</td>"   >> /workdir/submissions/$GROUP/index.html
-    echo "<td>Src</td>"      >> /workdir/submissions/$GROUP/index.html
-    echo "</tr>"             >> /workdir/submissions/$GROUP/index.html
+    echo "<tr>"              >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>Group</td>"    >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>Test</td>"     >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>OK</td>"       >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>Output</td>"   >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "<td>Src</td>"      >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "</tr>"             >> /workdir/results/$GROUP/index_$GROUP.html
     #######
 
 
     #######
-    echo "</table>"        >> /workdir/submissions/$GROUP/index.html
-    echo "</html>"         >> /workdir/submissions/$GROUP/index.html
+    echo "</table>"        >> /workdir/results/$GROUP/index_$GROUP.html
+    echo "</html>"         >> /workdir/results/$GROUP/index_$GROUP.html
     #######
 
     echo ""
     echo ""
-    echo "" >> /workdir/results/grades_$GROUP.csv
+    echo "" >> /workdir/results/$GROUP/grades_$GROUP.csv
 done
